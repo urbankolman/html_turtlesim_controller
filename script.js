20944ef507f257f6b9a863bbc1c15e1b72f90eac
urbankolman <143331035+urbankolman@users.noreply.github.com>
2023-08-27 15:28:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // odpri nov web socket na portu 9090 local host
-//const ws = new WebSocket('ws://localhost:9091');
+const ws = new WebSocket('ws://localhost:9091');
 // tukaj napišeš IP računalnika
 //const ws = new WebSocket('ws://172.20.10.8:9091');
-const ws = new WebSocket('ws://192.168.1.100:9091');
+//const ws = new WebSocket('ws://192.168.1.100:9091');
 
 
 // **********  INICIALIZACIJA widget-ov IZ HTML-JA **********************************
